d66fd9b6ecd8350fd59192cb1ed03ad85cb39c55
edu2ucoc <edu2.ucoc@gmail.com>
2025-12-30 13:50:19 +0900

    feat: 빼기 기능 추가

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ def add(a, b):
 # 곱하기 함수
 def mul(a, b):
     return a * b
+
+# 빼기  함수
+def sub(a, b):
+    return a - b
--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-from app import add, mul  # mul import 추가
+from app import add, mul, sub  # mul import 추가
 
 # 곱하기 테스트
 def test_add():
@@ -9,3 +9,8 @@ def test_add():
 def test_mul():
     assert mul(2, 3) == 6
     assert mul(0, 5) == 0
+
+# 빼기 테스트
+def test_sub():
+    assert sub(2, 3) == -1
+    assert sub(0, 5) == -5
